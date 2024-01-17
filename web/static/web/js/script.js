87110a4e55764216f5862ee0599fb34116287e8b
Reaien <ivanduarteherrera@gmail.com>
2024-01-16 22:31:48 -0300

    script obverser for section chile added

---
+++ web/static/web/js/script.js
@@ -0,0 +1,32 @@
+//fx para verificar si un elemento está en el area visible de la pantalla
+console.log('hola desde el js')
+
+// Configuración del Intersection Observer
+const opcionesObserver = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.02 // Umbral del 50%
+};
+
+//fx para manejar el desplazamiento y activar la animación
+
+function manejarEntradas(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const elemento = entry.target;
+            if (elemento.id === 'pais_chile') {
+                elemento.classList.add('fade-in');
+            }else if(elemento.id === 'cuequeros'){
+                elemento.classList.add('fade-in-right')
+            }
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+const observer = new IntersectionObserver(manejarEntradas, opcionesObserver);
+
+const idChile = document.getElementById('pais_chile');
+const idPareja = document.getElementById('cuequeros');
+observer.observe(idChile);
+observer.observe(idPareja);
