626ecb09be1f78cf28c7993430e376a0539b4080
Reaien <ivanduarteherrera@gmail.com>
2024-01-21 00:12:38 -0300

    colors margins - fixed news apart - card commented

--- web/views.py
+++ web/views.py
@@ -3,7 +3,7 @@ from .models import Noticia
 # Create your views here.
 def home(request):
     first_noticia = Noticia.objects.last()
-    old_noticia = Noticia.objects.all()[0:2]
+    old_noticia = Noticia.objects.all().order_by('-id')[1:5]
     return render(request, 'web/index.html',{
         'first_noticia': first_noticia,
         'old_noticia': old_noticia
