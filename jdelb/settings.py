1c93c26fcc4495610c258261258ca8ee2ee59a91
Reaien <ivanduarteherrera@gmail.com>
2025-03-12 18:50:45 -0300

    static_root para renderra

--- jdelb/settings.py
+++ jdelb/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR)
-STATIC_ROOT = os.path.join(BASE_DIR)  # Define la carpeta donde se recopilarán los archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Define la carpeta donde se recopilarán los archivos estáticos
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
